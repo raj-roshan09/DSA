9345e13c90fc1963e4801ee5fa7fe28107349c43
raj-roshan09 <roshan0918raj@gmail.com>
2026-08-08 15:16:38 +0530

    gave codeforces round 1116

---
+++ Contests/CodeForces/Round_1116/q_1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+int main() {
+    // fastio;
+    ll t;
+    cin >> t;
+    while (t--) {
+        
+    }
+    return 0;
+}
