89715404e6e6c86b1b544869d10dedcc45751458
raj-roshan09 <roshan0918raj@gmail.com>
2026-08-09 21:25:27 +0530

    codeforces_round 1116

--- Contests/CodeForces/Round_1116/q_1.cpp
+++ Contests/CodeForces/Round_1116/q_1.cpp
@@ -14,7 +14,14 @@ int main() {
     ll t;
     cin >> t;
     while (t--) {
-        
+        vector<int > a(3);
+        for(int i=0; i<3; i++){
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        int dif = a[2] - a[0];
+
+        cout << min(dif, a[1]) << endl;
     }
     return 0;
 }
--- Contests/CodeForces/Round_1116/q_2.cpp
+++ Contests/CodeForces/Round_1116/q_2.cpp
@@ -14,7 +14,39 @@ int main() {
     ll t;
     cin >> t;
     while (t--) {
-        
+        int n; 
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int even = 0;
+        bool ok1 = true;
+        bool ok2 = true;
+        for(int i=0; i<n; i+=2){
+            int pos = i/2;
+            char guess1 = (pos % 2 == 0) ? '0' : '1';
+            char guess2 = (pos % 2 == 0) ? '1' : '0';
+            if(s[i] != '?' && s[i] != guess1)    ok1 = false;
+            if(s[i] != '?' && s[i] != guess2)    ok2 = false;
+        }
+        if(ok1) even++;
+        if(ok2) even++;
+
+        int odd = 0;
+        ok1 = true;
+        ok2 = true;
+        for(int i=1; i<n; i+=2){
+            int pos = (i-1)/2;
+            char guess1 = (pos % 2 == 0) ? '0' : '1';
+            char guess2 = (pos % 2 == 0) ? '1' : '0';
+            if(s[i] != '?' && s[i] != guess1)   ok1 = false;
+            if(s[i] != '?' && s[i] != guess2)   ok2 = false;
+        }
+        if(ok1)    odd++;
+        if(ok2)    odd++;
+
+        cout << even * odd << endl;
     }
     return 0;
 }
