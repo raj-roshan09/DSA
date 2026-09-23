8f9c52f47388662d8e610f0a4f9b7a0c70116ae9
raj-roshan09 <roshan0918raj@gmail.com>
2026-09-23 22:49:21 +0530

    STARTERS-257

---
+++ Contests/Codechef/Starters 257(div.3)/q1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    int ans = n;
+	    while(k <= n && n != 0){
+	        n = n - k + 1;
+	        ans++;
+	        
+	    }
+	    cout << ans << endl;
+	}
+
+}
---
+++ Contests/Codechef/Starters 257(div.3)/q2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    vector<int > c(n);
+	    for(int i=0; i<n; i++){
+	        cin >> c[i];
+	    }
+	    
+	    int ans = INT_MAX;
+	    for(int i=0; i<n; i++){
+	        for(int j=i+1; j<n; j++){
+	            if (i <= k && j >= n-1-k && j-i <= 2*k+1){
+	                ans = min(ans, c[i] + c[j]);
+	            } 
+	        }
+	    }
+	    if(ans == INT_MAX)  cout << -1 << endl;
+	    else    cout << ans << endl;
+    }
+
+}
